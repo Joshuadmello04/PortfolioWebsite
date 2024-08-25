2f00dfbcb0bab70a0eef2185d864abb9c6f69327
Joshuadmello04 <joshuadmello777@gmail.com>
2024-08-25 23:14:14 +0530

    Added GitHub activity component with custom styling

--- src/components/Calender.jsx
+++ src/components/Calender.jsx
@@ -1,28 +1,47 @@
 import GitHubCalendar from "react-github-calendar";
-import { motion } from 'framer-motion'
+import { motion } from 'framer-motion';
+import { useEffect, useState } from "react";
 
 const Calendar = () => {
-  // Define custom colors
+  const [key, setKey] = useState(0);
+
+  // Set up polling to refresh the component every hour
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setKey(prevKey => prevKey + 1);
+    }, 3600000); // 1 hour
+
+    // Clean up the interval on component unmount
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="flex flex-col items-center p-6 rounded-lg shadow-lg">
       <motion.h2  
-        whileInView={{opacity:1,y:10}}
-        initial= {{opacity:0,y:-20}}
+        whileInView={{opacity:1, y:10}}
+        initial={{opacity:0, y:-20}}
         transition={{duration:0.75}}
-      className="mt-20 text-center bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 pb-8 bg-clip-text text-7xl tracking-tight text-transparent">GitHub Activity</motion.h2>
+        className="mt-20 text-center bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 pb-8 bg-clip-text text-7xl tracking-tight text-transparent"
+      >
+        GitHub Activity
+      </motion.h2>
+      
       <motion.p 
-      whileInView={{opacity:1,y:10}}
-        initial= {{opacity:0,y:-20}}
+        whileInView={{opacity:1, y:10}}
+        initial={{opacity:0, y:-20}}
         transition={{duration:0.75}}
-      className="text-xl my-10 mb-11">
-Check out my <span className=" text-2xl bg-gradient-to-r from-green-300 to-green-600 bg-clip-text text-transparent">Github</span> contributions.
+        className="text-xl my-10 mb-11"
+      >
+        Check out my <span className="text-2xl bg-gradient-to-r from-green-300 to-green-600 bg-clip-text text-transparent">GitHub</span> contributions.
       </motion.p>
+      
       <GitHubCalendar
-        className="m-10 text-pink-300 "
+        key={key} // Key to trigger re-render
+        className="m-10 text-pink-300"
         username="joshuadmello04"
         colorScheme="dark"
         blockSize={20}
-        fontSize={17}
+        fontSize={20}
       />
     </div>
   );
